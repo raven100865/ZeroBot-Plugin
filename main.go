7429102f5bbcbb8eedee95679a5e21467a1ed3a1
raven100865 <79786881+raven100865@users.noreply.github.com>
2022-09-23 17:19:56 +0800

    name

--- main.go
+++ main.go
@@ -216,8 +216,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
-	// sus = append(sus, 87654321)
+	 sus = append(sus, 1466276741)
+	 sus = append(sus, 3585216020)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
@@ -240,7 +240,7 @@ func init() {
 
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:      append([]string{*adana}, "ATRI", "atri", "亚托莉", "アトリ"),
+		NickName:      append([]string{*adana}, "ATRI", "atri", "亚托莉", "アトリ","鸭鸭"),
 		CommandPrefix: *prefix,
 		SuperUsers:    sus,
 		Driver:        []zero.Driver{config.W[0]},
